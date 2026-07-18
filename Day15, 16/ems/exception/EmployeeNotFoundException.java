51620d50e59b5e514eae0fc7795fd5f202076188
chityalasrakshin <bsrakshin@gmail.com>
2026-07-18 22:53:40 +0530

    15,16

---
+++ Day15, 16/ems/exception/EmployeeNotFoundException.java
@@ -0,0 +1,12 @@
+package com.coforge.ems.exception;
+
+
+
+public class EmployeeNotFoundException extends Exception{
+
+	@Override
+	public String toString() {
+		return "FAILED : Employee Not Found";
+	}
+	
+}
---
+++ Day15, 16/ems/exception/InvalidEmployeeObjectException.java
@@ -0,0 +1,12 @@
+package com.coforge.ems.exception;
+
+
+
+public class InvalidEmployeeObjectException extends Exception{
+
+	@Override
+	public String toString() {
+		return "FAILED : Invalid Employee Details";
+	}
+	
+}
